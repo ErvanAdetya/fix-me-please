cc3c18464f5bd09cf69c2d18d7abf5a7ec10fc26
Ervan Adetya <vaanz.adetya@gmail.com>
2018-03-28 10:05:57 +0700

    Hopefully no bugs

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const app = express;
+const bodyParser =  require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
 
 const books = require('./routes/books');
 var transactions = require('./routes/transactions');
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
+// mongoose.connect('mongodb://localhost/api-crud-mongoose', {useMongoClient: true},(err) => {
+//   err ? console.log('Can\'t connect to database') : console.log('Database connected')
+// });
+mongoose.connect('mongodb://localhost:27017/flcode', {useMongoClient: true},(err) => {
   err ? console.log('Can\'t connect to database') : console.log('Database connected')
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/books', books);
 app.use('/transactions', transactions);
